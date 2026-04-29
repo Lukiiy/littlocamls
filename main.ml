b012ad25b4797105b8a4122c6e59a2901f7aa6be
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-04-29 11:16:16 -0300

    fizzbuzz with 128

--- main.ml
+++ main.ml
@@ -1,1 +1,18 @@
-print_endline "Hello, World!"
+let fizzbuzz i =
+  if i mod 15 = 0 then
+    Printf.printf "FizzBuzz (%d)\n" i
+  else if i mod 3 = 0 then
+    Printf.printf "Fizz (%d)\n" i
+  else if i mod 5 = 0 then
+    Printf.printf "Buzz (%d)\n" i
+  else
+    print_endline (string_of_int i)
+
+let () =
+  print_endline "It'z fizzbuzz time!";
+
+  for i = 1 to 128 do
+    fizzbuzz i
+  done;
+
+  print_endline "wow!!";
