2f14657b4537e1d1efb7433e550bb51a103f40ac
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-02 17:37:42 -0300

    Create palindrome.ml

---
+++ palindrome.ml
@@ -0,0 +1,19 @@
+let normalize string =
+  string
+  |> String.lowercase_ascii (* lowercase it *)
+  |> String.to_seq (* turn into a sequence of characters *)
+  |> Seq.filter (fun char ->
+       ('a' <= char && char <= 'z') || ('0' <= char && char <= '9')) (* keep only letters and digits *)
+  |> String.of_seq (* build it *)
+
+let is_palindrome s =
+  let string = normalize s in
+
+  (* recursive loop that checks if letters from one end to another differ or don't *)
+  let rec loop x y =
+    if x >= y then true (* all letters matched, true *)
+    else if string.[x] <> string.[y] then false (* if chars diff, false *)
+    else loop (x + 1) (y - 1) (* get letters *)
+  in
+
+  loop 0 (String.length string - 1)
