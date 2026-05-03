000fb2487e68cd037d9b6724b02ade5e12314a58
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-02 21:56:37 -0300

    ascii note + let ()
    * Added note about lowercase_ascii
    * Added let () that asks for an input to check

--- palindrome.ml
+++ palindrome.ml
@@ -1,6 +1,6 @@
 let normalize string =
   string
-  |> String.lowercase_ascii (* lowercase it *)
+  |> String.lowercase_ascii (* lowercase it and filter out non-alphanumeric characters *)
   |> String.to_seq (* turn into a sequence of characters *)
   |> Seq.filter (fun char ->
        ('a' <= char && char <= 'z') || ('0' <= char && char <= '9')) (* keep only letters and digits *)
@@ -17,3 +17,13 @@ let is_palindrome s =
   in
 
   loop 0 (String.length string - 1)
+
+let () =
+  print_string "Check if a string is a palindrome: ";
+
+  let input = read_line () in
+
+  if is_palindrome input then
+    print_endline "It is a palindrome!"
+  else
+    print_endline "It isn't a palindrome."
