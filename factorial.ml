158356d2e081334fe9eaf2fdccc9469682c44ec6
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-03 17:51:40 -0300

    Updated factorial.ml
    * Added dependency Zarith
    * Refactored it to calculate with Zarith

--- factorial.ml
+++ factorial.ml
@@ -1,6 +1,13 @@
-let rec factorial n =
-  if n <= 1 then 1
-  else n * factorial (n - 1)
+open Z
+
+let factorial n =
+  let acc = ref one in
+
+  for i = 2 to n do
+    acc := mul !acc (of_int i)
+  done;
+
+  !acc
 
 let () =
   if Array.length Sys.argv < 2 then (
@@ -10,7 +17,7 @@ let () =
 
   match int_of_string_opt Sys.argv.(1) with
     | Some integer ->
-      Printf.printf "Factorial of %d is %d\n" integer (factorial integer)
+      Printf.printf "Factorial of %d is %s\n" integer (to_string (factorial integer))
     | None ->
       prerr_endline "Argument must be an int";
       exit 1
